1c832e5e9915a1b770be7383a4d836b2533632dc
Ali Kazmi <ali.kazmi@10pearls.com>
2021-03-11 01:17:45 +0500

    initial checkin

---
+++ index.js
@@ -0,0 +1,172 @@
+const axios = require("axios").default;
+const WebSocket = require("ws");
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
+const { exec } = require("child_process");
+
+const winToSocket = {};
+const port = 31337;
+const debuggerUrl = `http://localhost:${port}`;
+axios.defaults.baseURL = debuggerUrl;
+
+const script = fs.readFileSync(path.join(__dirname, "script.js"), {
+  encoding: "utf-8",
+});
+
+async function sleep(durationMs) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), durationMs);
+  });
+}
+
+async function findChatWindow() {
+  let windowFound = false;
+
+  while (!windowFound) {
+    let response;
+    try {
+      response = await axios.get("/json/list");
+    } catch {}
+
+    if (response) {
+      for (const win of response.data) {
+        if (win.url.indexOf("/conversations/") >= 0) {
+          console.log("window found");
+          return win;
+        }
+      }
+    }
+
+    console.log("windows not found. waiting...");
+    await sleep(5000);
+  }
+}
+
+function createEvalExpression(expression) {
+  return {
+    id: 1337,
+    method: "Runtime.evaluate",
+    params: {
+      expression,
+      objectGroup: "evalme",
+      returnByValue: false,
+      userGesture: true,
+    },
+  };
+}
+
+async function initWebSocket(wsUrl) {
+  return new Promise((resolve) => {
+    const ws = new WebSocket(wsUrl);
+    ws.once("open", () => {
+      resolve(ws);
+    });
+  });
+}
+
+async function getSocketByWindow(window) {
+  if (!winToSocket[window.id]) {
+    const ws = await initWebSocket(window.webSocketDebuggerUrl);
+    winToSocket[window.id] = ws;
+  }
+
+  return winToSocket[window.id];
+}
+
+async function receiveWsResponse(ws) {
+  return new Promise((resolve) => {
+    ws.once("message", (data) => {
+      resolve(data);
+    });
+  });
+}
+
+async function isRunningWithDebugger() {
+  try {
+    await axios.get("/json/list");
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+async function runProcess(cmdString, workingDirectory) {
+  return new Promise((resolve, reject) => {
+    let opts = {};
+    if (workingDirectory) {
+      opts = { cwd: workingDirectory };
+    }
+    exec(cmdString, opts, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        reject(error);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        reject(error);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      resolve();
+    });
+  });
+}
+
+async function ensureWindowsProcess() {
+  //kill existing process
+  try {
+    await runProcess("taskkill /f /im Teams.exe");
+  } catch {}
+
+  //get teams path
+  const teamsPath = path.join(
+    process.env.LOCALAPPDATA,
+    "Microsoft",
+    "Teams",
+    "Update.exe"
+  );
+
+  //launch process
+  const cmd = `${teamsPath} --processStart Teams.exe --process-start-args --remote-debugging-port=${port}`;
+  console.log({ cmd });
+  await runProcess(cmd, path.dirname(cmd));
+}
+
+async function ensureMacProcess() {
+  //kill existing process
+  try {
+    await runProcess("killall Teams");
+  } catch {}
+
+  //get teams path
+  const teamsPath = "/Applications/Microsoft Teams.app/Contents/MacOS/Teams";
+
+  //launch process
+  const cmd = `${teamsPath} --remote-debugging-port=${port}`;
+  console.log({ cmd });
+  await runProcess(cmd, path.dirname(cmd));
+}
+
+(async () => {
+  if (!(await isRunningWithDebugger())) {
+    if (os.platform() === "win32") {
+      ensureWindowsProcess();
+    } else if (os.platform() === "darwin") {
+      ensureMacProcess();
+    } else {
+      console.warn(
+        `unsupported os ${os.platform()}. only compatible with windows and mac. exiting...`
+      );
+      process.exit();
+    }
+  }
+
+  const chatWindow = await findChatWindow();
+  const payload = createEvalExpression(script); //createEvalExpression("alert('hello world')");
+  const ws = await getSocketByWindow(chatWindow);
+  ws.send(JSON.stringify(payload));
+  const result = await receiveWsResponse(ws);
+  console.log({ result });
+})();
---
+++ script.js
@@ -0,0 +1,178 @@
+function addQuoteMessage() {
+  const threadBodyMediaList = document.querySelectorAll(
+    '[data-tid="threadBodyMedia"]'
+  );
+
+  if (threadBodyMediaList) {
+    threadBodyMediaList.forEach((threadBody) => {
+      const threadBodyTopRow = threadBody.querySelector(
+        ".top-row-text-container"
+      );
+
+      if (threadBody && !threadBodyTopRow.hasAttribute("data-hasquote")) {
+        if (threadBodyTopRow) {
+          const quoteMessageButton = document.createElement("button");
+          quoteMessageButton.classList.add("teams-custom-quote-message-button");
+          quoteMessageButton.innerText = "Reply";
+          Object.assign(quoteMessageButton.style, quoteMessageButtonStyles);
+          threadBodyTopRow.appendChild(quoteMessageButton);
+          threadBodyTopRow.setAttribute("data-hasquote", "true");
+          quoteMessageButton.addEventListener(
+            "click",
+            quoteMessageButtonClickHandler
+          );
+        }
+      }
+    });
+  }
+}
+
+function quoteMessageButtonClickHandler(event) {
+  if (!composeHasActiveQuoteMessage()) {
+    const messageText = event.target.offsetParent.querySelector(
+      '[data-tid="messageBodyContent"]'
+    ).innerText;
+    const messageSenderName = event.target.offsetParent.querySelector(
+      '[data-tid="threadBodyDisplayName"]'
+    ).innerText;
+
+    const blockquote = document.createElement("blockquote");
+    blockquote.setAttribute("itemtype", "http://schema.skype.com/Reply");
+
+    const topRowParagraph = document.createElement("p");
+    blockquote.appendChild(topRowParagraph);
+
+    const topRowParagraphBreak = document.createElement("b");
+    topRowParagraphBreak.style.display = "flex";
+    topRowParagraphBreak.style.justifyContent = "space-between";
+    topRowParagraph.appendChild(topRowParagraphBreak);
+
+    const nameSpan = document.createElement("span");
+    const closeSpan = document.createElement("span");
+    nameSpan.style.fontSize = "small";
+    nameSpan.innerText = messageSenderName.trim();
+    closeSpan.innerText = "x";
+    closeSpan.style.cursor = "pointer";
+    closeSpan.classList.add("teams-custom-quote-message-close");
+    closeSpan.addEventListener("click", replyMessageClickHandler);
+    topRowParagraphBreak.appendChild(nameSpan);
+    topRowParagraphBreak.appendChild(closeSpan);
+
+    const textPreviewParagraph = document.createElement("p");
+    textPreviewParagraph.innerText = messageText;
+    blockquote.appendChild(textPreviewParagraph);
+
+    const mainQuoteDiv = document.createElement("div");
+    mainQuoteDiv.appendChild(blockquote);
+
+    const composeTextarea = document.querySelector(".cke_wysiwyg_div");
+    const composePlaceholder = document.querySelector(".ts-text-watermark");
+    composePlaceholder.style.display = "none";
+    composeTextarea.insertBefore(mainQuoteDiv, composeTextarea.firstChild);
+
+    const composerParent = document.querySelector(".cke_contents");
+    composerParent.style.height = blockquote.offsetHeight + 40 + "px";
+  }
+}
+
+function replyMessageClickHandler() {
+  const compose = document.querySelector(".cke_wysiwyg_div");
+  const composeParent = document.querySelector(".cke_contents");
+  const composePlaceholder = document.querySelector(".ts-text-watermark");
+
+  compose.innerHTML = "<div> <br> </div>";
+  composePlaceholder.style.display = "block";
+  composeParent.style.height = "33px";
+}
+
+function sendButtonClickHandler() {
+  const replyMessageCloseButton = document.querySelector(
+    ".teams-custom-quote-message-close"
+  );
+
+  if (replyMessageCloseButton) {
+    replyMessageCloseButton.innerText = "";
+  }
+}
+
+function composeHasActiveQuoteMessage() {
+  return document.querySelector(".cke_wysiwyg_div").querySelector("blockquote");
+}
+
+function appendCustomStyles() {
+  const head = document.getElementsByTagName("head")[0];
+  const style = document.createElement("style");
+  const declarations = document.createTextNode(`
+     .teams-custom-quote-message-button:hover { color: #a2c0f5 !important; }
+     .teams-custom-quote-message-close:hover { opacity: .5 !important; }
+     .ts-message-thread-body .message-body-top-row .top-row-text-container { justify-content: space-between; }
+  `);
+
+  style.type = "text/css";
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = declarations.nodeValue;
+  } else {
+    style.appendChild(declarations);
+  }
+
+  head.appendChild(style);
+}
+
+const quoteMessageButtonStyles = {
+  margin: "0 0 0 2em",
+  padding: "0",
+  background: "transparent",
+  outline: "none",
+  border: "none",
+  cursor: "pointer",
+  fontWeight: "bold",
+  color: "cornflowerblue",
+  textDecoration: "underline",
+};
+
+function initializeDOMChangeListener(observeTarget) {
+  const config = {
+    attributes: false,
+    childList: true,
+    subtree: true,
+    characterData: false,
+    characterDataOldValue: false,
+    attributeOldValue: false,
+  };
+
+  const mutationObserverCallback = function (mutations, observer) {
+    if (mutations) {
+      for (let i = 0; i < mutations.length; i++) {
+        if (mutations[i].addedNodes) {
+          for (let x = 0; x < mutations[i].addedNodes.length; x++) {
+            if (
+              mutations[i].addedNodes[x].classList &&
+              mutations[i].addedNodes[x].classList.contains("thread-body")
+            ) {
+              addQuoteMessage();
+            }
+          }
+        }
+      }
+    }
+  };
+
+  const observer = new MutationObserver(mutationObserverCallback);
+  observer.observe(observeTarget, config);
+  console.log("DOM Mutation Observer init");
+}
+
+setTimeout(() => {
+  const observeTarget =
+    document.querySelector(".ts-message-content") || document;
+  const sendButton = document.querySelector(".compose-send-discard");
+
+  if (sendButton) {
+    sendButton.addEventListener("click", sendButtonClickHandler);
+  }
+
+  appendCustomStyles();
+  addQuoteMessage();
+  initializeDOMChangeListener(observeTarget);
+}, 4000);
